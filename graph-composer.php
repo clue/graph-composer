d4e9482d689251281d348cd921341d90c5fa9e27
Christian Lück <christian@lueck.tv>
2013-05-15 12:24:15 +0200

    Fix classname reference

--- graph-composer.php
+++ graph-composer.php
@@ -5,5 +5,5 @@ require_once __DIR__ . '/vendor/autoload.php';
 // directory to scan
 $dir = isset($argv[1]) ? $argv[1] : __DIR__;
 
-$graph = new Graph();
+$graph = new Clue\GraphComposer();
 $graph->displayGraph($dir);
