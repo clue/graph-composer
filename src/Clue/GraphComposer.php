3fc8bc68a05fce1da3f4a38e1316fe5289668034
Christian Lück <christian@lueck.tv>
2015-11-16 20:26:36 +0100

    Update to clue/graph v0.9.0 and graphp/graphviz v0.2.0
    This improves error reporting and fixes several compatibility issues
    with MS Windows

--- src/Clue/GraphComposer.php
+++ src/Clue/GraphComposer.php
@@ -2,8 +2,10 @@
 
 namespace Clue;
 
-use Fhaculty\Graph\GraphViz;
 use Fhaculty\Graph\Graph;
+use Fhaculty\Graph\Attribute\AttributeAware;
+use Fhaculty\Graph\Attribute\AttributeBagNamespaced;
+use Graphp\GraphViz\GraphViz;
 
 class GraphComposer
 {
@@ -13,27 +15,27 @@ class GraphComposer
         'shape' => 'box',
         'fontcolor' => '#314B5F'
     );
-    
+
     private $layoutVertexRoot = array(
         'style' => 'filled, rounded, bold'
     );
-    
+
     private $layoutEdge = array(
         'fontcolor' => '#767676',
         'fontsize' => 10,
         'color' => '#1A2833'
     );
-    
+
     private $layoutEdgeDev = array(
         'style' => 'dashed'
     );
-    
+
     private $dependencyGraph;
-    
+
     private $format = 'svg';
-    
+
     /**
-     * 
+     *
      * @param string $dir
      */
     public function __construct($dir)
@@ -41,65 +43,75 @@ class GraphComposer
         $analyzer = new \JMS\Composer\DependencyAnalyzer();
         $this->dependencyGraph = $analyzer->analyze($dir);
     }
-    
+
     /**
-     * 
+     *
      * @param string $dir
      * @return \Fhaculty\Graph\Graph
      */
     public function createGraph()
     {
         $graph = new Graph();
-        
+
         foreach ($this->dependencyGraph->getPackages() as $package) {
             $name = $package->getName();
             $start = $graph->createVertex($name, true);
-            
+
             $label = $name;
             if ($package->getVersion() !== null) {
                 $label .= ': ' . $package->getVersion();
             }
-            
-            $start->setLayout(array('label' => $label) + $this->layoutVertex);
-        
+
+            $this->setLayout($start, array('label' => $label) + $this->layoutVertex);
+
             foreach ($package->getOutEdges() as $requires) {
                 $targetName = $requires->getDestPackage()->getName();
                 $target = $graph->createVertex($targetName, true);
-                
+
                 $label = $requires->getVersionConstraint();
-                
-                $edge = $start->createEdgeTo($target)->setLayout(array('label' => $label) + $this->layoutEdge);
-                
+
+                $edge = $start->createEdgeTo($target);
+                $this->setLayout($edge, array('label' => $label) + $this->layoutEdge);
+
                 if ($requires->isDevDependency()) {
-                    $edge->setLayout($this->layoutEdgeDev);
+                    $this->setLayout($edge, $this->layoutEdgeDev);
                 }
             }
         }
 
-        $graph->getVertex($this->dependencyGraph->getRootPackage()->getName())->setLayout($this->layoutVertexRoot);
-        
+        $root = $graph->getVertex($this->dependencyGraph->getRootPackage()->getName());
+        $this->setLayout($root, $this->layoutVertexRoot);
+
         return $graph;
     }
-        
+
+    private function setLayout(AttributeAware $entity, array $layout)
+    {
+        $bag = new AttributeBagNamespaced($entity->getAttributeBag(), 'graphviz.');
+        $bag->setAttributes($layout);
+
+        return $entity;
+    }
+
     public function displayGraph()
     {
         $graph = $this->createGraph();
-        
-        $graphviz = new GraphViz($graph);
+
+        $graphviz = new GraphViz();
         $graphviz->setFormat($this->format);
-        $graphviz->display();
+        $graphviz->display($graph);
     }
-    
+
     public function getImagePath()
     {
         $graph = $this->createGraph();
-        
-        $graphviz = new GraphViz($graph);
+
+        $graphviz = new GraphViz();
         $graphviz->setFormat($this->format);
-        
-        return $graphviz->createImageFile();
+
+        return $graphviz->createImageFile($graph);
     }
-    
+
     public function setFormat($format)
     {
         $this->format = $format;
